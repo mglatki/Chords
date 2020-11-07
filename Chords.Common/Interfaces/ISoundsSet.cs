df8eba23de7872170fa8bad781d19febb596c613
mglatki <mglatki@gmail.com>
2020-11-07 11:59:14 +0100

    Improved SoundsSet model and interface

--- Chords.Common/Interfaces/ISoundsSet.cs
+++ Chords.Common/Interfaces/ISoundsSet.cs
@@ -4,6 +4,12 @@ namespace Chords.Common.Interfaces
 {
     public interface ISoundsSet
     {
+        string bassEStringFretPushed { get; set; }
+        string aStringFretPushed { get; set; }
+        string dStringFretPushed { get; set; }
+        string gStringFretPushed { get; set; }
+        string hStringFretPushed { get; set; }
+        string eStringFretPushed { get; set; }
         string[,] GetSounds();
         void SetSounds(string[,] value);
     }
--- Chords.Common/Models/SoundsSet.cs
+++ Chords.Common/Models/SoundsSet.cs
@@ -15,16 +15,36 @@ namespace Chords.Common.Models
             this.eStringFretPushed = eStringFretPushed;
 
         }
-        public string bassEStringFretPushed { get; private set; }
-        public string aStringFretPushed { get; private set; }
-        public string dStringFretPushed { get; private set; }
-        public string gStringFretPushed { get; private set; }
-        public string hStringFretPushed { get; private set; }
-        public string eStringFretPushed { get; private set; }
+
+        public SoundsSet(ISoundsSet soundsSet)
+        {
+            this.bassEStringFretPushed = soundsSet.bassEStringFretPushed;
+            this.aStringFretPushed = soundsSet.aStringFretPushed;
+            this.dStringFretPushed = soundsSet.dStringFretPushed;
+            this.gStringFretPushed = soundsSet.gStringFretPushed;
+            this.hStringFretPushed = soundsSet.hStringFretPushed;
+            this.eStringFretPushed = soundsSet.eStringFretPushed;
+
+        }
+
+        public string bassEStringFretPushed { get; set; }
+        public string aStringFretPushed { get; set; }
+        public string dStringFretPushed { get; set; }
+        public string gStringFretPushed { get; set; }
+        public string hStringFretPushed { get; set; }
+        public string eStringFretPushed { get; set; }
         public string[,] GetSounds()
         {
 
-            string[,] arr = new string[2, 2] { { "E", "2" }, { "A", "1" } };
+            string[,] arr = new string[6, 2] 
+                {   
+                    { "E", bassEStringFretPushed }, 
+                    { "A", aStringFretPushed }, 
+                    { "D", dStringFretPushed } , 
+                    { "G", gStringFretPushed } , 
+                    { "H", hStringFretPushed } , 
+                    { "E", eStringFretPushed }  
+                };
 
             return arr;
         }
